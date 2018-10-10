8fe307314363f857c7240b6aa90c99b56972b957
Shamsutdinov Ruslan <kit4curs@gmail.com>
2018-10-10 20:15:12 +0500

    Test commit

--- app/src/main/java/com/happinessfoot/test/MainActivity.java
+++ app/src/main/java/com/happinessfoot/test/MainActivity.java
@@ -1,13 +1,42 @@
 package com.happinessfoot.test;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            XmlPullParser parser = getResources().getXml(R.xml.test);
+
+            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                final String TAG = "ЛогКот";
+                String tmp = "";
+
+                parser.next();
+            }
+        } catch (Throwable t) {
+            Toast.makeText(this,
+                    "Ошибка при загрузке XML-документа: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/happinessfoot/test/products.java
@@ -0,0 +1,9 @@
+package com.happinessfoot.test;
+
+/**
+ * Created by rusli_000 on 10.10.2018.
+ */
+
+public class products {
+
+}
