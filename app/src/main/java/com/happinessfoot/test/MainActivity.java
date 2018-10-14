bf040331cb2f01f1d925c877dd7b378f9135c9a1
Shamsutdinov Ruslan <kit4curs@gmail.com>
2018-10-14 17:33:13 +0500

    Main activity changes

--- app/src/main/java/com/happinessfoot/test/MainActivity.java
+++ app/src/main/java/com/happinessfoot/test/MainActivity.java
@@ -1,11 +1,13 @@
 package com.happinessfoot.test;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -23,12 +25,42 @@ import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnViewProducts;
+    Button btnNewProduct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
 
+        // обработчик на нажатиЯ кнопки View Products
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Запускаем Activity вывода всех продуктов
+                Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        // обработчик на нажатия кнопки Add New Products
+        btnNewProduct.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Запускаем Activity создания нового продукта
+                Intent i = new Intent(getApplicationContext(), NewProductActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
